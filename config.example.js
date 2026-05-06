6dcf2998a7731ca9a42fdcd6ff0395375b24c9db
ardianrifendy <91584586+ardianrifendy@users.noreply.github.com>
2026-05-06 09:16:17 +0700

    Add files via upload

---
+++ config.example.js
@@ -0,0 +1,13 @@
+export default {
+    // Delay antar aksi di dalam grup (dalam milidetik)
+    // Cth: 2000 = 2 detik
+    delayTyping: 100, // delay tiap huruf saat ngetik (ms)
+    delayAction: 3000, // delay sebelum klik post dll
+
+    // Delay antar grup saat posting massal (dalam milidetik)
+    // Sangat disarankan > 1 menit (60000) untuk meminimalisir banned
+    delayBetweenGroups: 60000, // Minimal 60 detik untuk keamanan
+    
+    // Teks default jika Anda tidak ingin mengisi manual terus menerus
+    defaultText: `Halo, ready stock! Silakan diorder 🔥\nBisa COD!`
+};
